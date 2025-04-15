3ed217e1aa35f08d1bf3df1767650315ce6cd7fb
ivan liu <ivan.liuyanfeng@gmail.com>
2025-04-15 22:54:08 +1000

    init

---
+++ src/discord.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+token = os.getenv("DISCORD_TOKEN")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(token)
---
+++ src/discord_last_messages.py
@@ -0,0 +1,27 @@
+import discord
+import time
+import asyncio
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+last_messages = 5
+token = os.getenv("DISCORD_TOKEN")
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    channels = ["general"]
+
+    if str(message.channel) in channels:
+        if message.content == "!history":
+            messages = await message.channel.history(limit=last_messages).flatten()
+            messages.reverse()
+
+            await message.channel.send(f"Last {last_messages} messages:")
+            await message.channel.send([message.content for message in messages])
+
+client.run(token)
